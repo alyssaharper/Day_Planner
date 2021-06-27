c6e84cd94ef935b360dbf8f10e54ab29ecd1bd1f
Alyssa harper <alyssaharper@Alyssas-Air.home>
2021-06-27 13:40:04 -0400

    Added JS functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,33 @@
 var today = moment().format("dddd MMMM Do, YYYY");
 $("#currentDay").text(today);
 
-var containerEl = $("#container");
-var timeBlockEl = $('<p>');
-timeBlockEl.appendTo(containerEl);
+var saveBtnEl = $(".saveBtn");
+var hourEl = saveBtnEl.parent(); 
+var descriptionEl = $(".description");
 
+
+function timeBlock() {
+    
+    
+}
+
+function clickSave(event) {
+    event.preventDefault();
+
+    var dayPlanner = {
+        time: hourEl.value,
+        description: descriptionEl.value
+    }
+
+    localStorage.setItem("dayPlanner", JSON.stringify(dayPlanner));
+    refresh();
+}
+
+function refresh() {
+    var newDay = JSON.parse(localStorage.getItem("dayPlanner"));
+    if (newDay !== null) {
+        descriptionEl.textContent = newDay.description;
+    }
+}
+
+saveBtnEl.on('click', clickSave);
